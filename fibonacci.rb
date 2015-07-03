bf71696a4b3eef22eb19ebe81c51f9e99056e86a
Cody Hacker <jbalsamo@cox.net>
2015-07-03 08:45:27 -0700

    finished

---
+++ fibonacci.rb
@@ -0,0 +1,19 @@
+def fibs n
+  return 0 if n == 0
+  num1 = 0
+  num2 = 1
+  n.times do
+    num1, num2 = num2, (num1+num2)
+  end
+  num1
+end
+
+p fibs(6)
+
+def fib_rec(n)
+  n <=1 ? n : fib_rec(n - 2) + fib_rec(n - 1)
+end
+
+p fib_rec(6)
+
+
